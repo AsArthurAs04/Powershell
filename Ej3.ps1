68ba63d1fa7d975c0f2108f1f381f91af085aabf
AsArthurAs04 <146834833+AsArthurAs04@users.noreply.github.com>
2026-01-18 23:02:49 +0100

    Add files via upload

---
+++ Ej3.ps1
@@ -0,0 +1,11 @@
+﻿Clear-Host
+[int]$num1 = Read-Host "Introduce el primer número"
+[int]$num2 = Read-Host "Introduce el segundo número"
+
+if ($num1 -gt $num2) {
+    Write-Host "El primero es MAYOR que el segundo."
+} ElseIf ($num1 -lt $num2) {
+    Write-Host "El primero es MENOR que el segundo."
+} Else {
+    Write-Host "Ambos números son IGUALES."
+}
---
+++ Ej9.ps1
@@ -0,0 +1,14 @@
+﻿Clear-Host
+[double]$renta = Read-Host "Introduce tu renta anual"
+
+if ($renta -lt 10000) {
+    Write-Host "Te corresponde un 5%"
+} ElseIf ($renta -lt 20000) {
+    Write-Host "Te corresponde un 15%"
+} ElseIf ($renta -lt 35000) {
+    Write-Host "Te corresponde un 20%"
+} ElseIf ($renta -lt 60000) {
+    Write-Host "Te corresponde un 30%"
+} Else {
+    Write-Host "Te corresponde un 45%"
+}
