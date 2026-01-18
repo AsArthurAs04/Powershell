5cc3cd43928c95b8f14e10dc2ec08ec05301d411
AsArthurAs04 <146834833+AsArthurAs04@users.noreply.github.com>
2026-01-18 22:54:47 +0100

    Replace cls with Clear-Host in Ejercicio1.ps1

--- Ejercicio1.ps1
+++ Ejercicio1.ps1
@@ -1,5 +1,6 @@
-﻿cls
+﻿Clear-Host
 Write-Host "Hola $env:USERNAME"
 Write-Host "Tu directorio de trabajo es $HOME"
 Write-Host "Perteneces al dominio $env:USERDOMAIN"
 Write-Host "Tu equipo se llama $env:COMPUTERNAME"
+
