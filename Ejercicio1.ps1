ebb54c6458ec72c68ef5a52ab010f7263dfde1b2
AsArthurAs04 <146834833+AsArthurAs04@users.noreply.github.com>
2026-01-18 22:50:04 +0100

    Add files via upload

---
+++ Ejercicio1.ps1
@@ -0,0 +1,5 @@
+﻿cls
+Write-Host "Hola $env:USERNAME"
+Write-Host "Tu directorio de trabajo es $HOME"
+Write-Host "Perteneces al dominio $env:USERDOMAIN"
+Write-Host "Tu equipo se llama $env:COMPUTERNAME"
---
+++ Ejercicio2.ps1
@@ -0,0 +1,15 @@
+﻿Clear-Host
+[int]$num1 = Read-Host "Escribe el primer numero"
+[int]$num2 = Read-Host "Escribe el segundo numero"
+$suma = $num1 + $num2
+$resta = $num1 - $num2 
+$multiplicacion = $num1 * $num2
+$division = $num1 / $num2
+$resto = $num1 % $num2
+
+Write-Host "El resultado de la suma es: $suma"
+Write-Host "El resultado de la resta es: $resta"
+Write-Host "El resultado de la multiplicación es: $multiplicacion"
+Write-Host "El resultado de la division es: $division"
+Write-Host "El resultado del resto  es: $resto"
+
