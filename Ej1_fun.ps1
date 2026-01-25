7d89a083feeea8047a6c435e48e2e291b2a39b31
AsArthurAs04 <146834833+AsArthurAs04@users.noreply.github.com>
2026-01-25 23:14:38 +0100

    Add files via upload

---
+++ Ej1_fun.ps1
@@ -0,0 +1,54 @@
+﻿Function Sumar {
+    param ($a, $b)
+    $resultado = $a + $b
+    Write-Host "Resultado: $resultado"
+}
+
+Function Restar {
+    param ($a, $b)
+    $resultado = $a - $b
+    Write-Host "Resultado: $resultado"
+}
+
+Function Multiplicar {
+    param ($a, $b)
+    $resultado = $a * $b
+    Write-Host "Resultado: $resultado"
+}
+
+Function Dividir {
+    param ($a, $b)
+    $resultado = $a / $b
+    Write-Host "Resultado: $resultado"
+}
+
+do {
+    Clear-Host
+    Write-Host "****** CALCULADORA ********"
+    Write-Host "1. Sumar"
+    Write-Host "2. Restar"
+    Write-Host "3. Multiplicar"
+    Write-Host "4. Dividir"
+    Write-Host "5. Salir"
+    
+    $opcion = Read-Host "¿Qué desea hacer? Elige una opción"
+
+
+    if ($opcion -ne 5) {
+        [int]$num1 = Read-Host "Escribe el primer número"
+        [int]$num2 = Read-Host "Escribe el segundo número"
+
+
+        switch ($opcion) {
+            1 { Sumar $num1 $num2 }
+            2 { Restar $num1 $num2 }
+            3 { Multiplicar $num1 $num2 }
+            4 { Dividir $num1 $num2 }
+            default { Write-Host "Opción no reconocida" }
+        }
+        
+
+        Read-Host "Pulsa Enter para continuar"
+    }
+
+} until ($opcion -eq 5) 
---
+++ Ej2_fun.ps1
@@ -0,0 +1,15 @@
+﻿
+Function Mostrar-Usuarios {
+    
+
+    $listaUsuarios = Import-Csv "usuarios.csv"
+
+
+    foreach ($usuario in $listaUsuarios) {
+        
+
+        Write-Host "Nombre: $($usuario.Nombre) - Apellidos: $($usuario.Apellidos) - Grupo: $($usuario.Grupo)"
+    }
+}
+
+Mostrar-Usuarios
